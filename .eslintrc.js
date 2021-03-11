e36cc01ac0a349bbf86dc218455643bd1ea391fe
Alvis Lu <uualvislu@gmail.com>
2021-03-11 18:24:52 +0800

    Inital sturct

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+  },
+  rules: {
+    'import/extensions': 'off',
+    'import/no-unresolved': 'error',
+    'no-console': 'off',
+  },
+};
